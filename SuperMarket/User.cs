c73a359e16f1200031447ce9a72175f8773f9ec5
= <=>
2025-01-13 20:44:30 +0300

    A simple user(ID, username) class with constructor and getters

---
+++ SuperMarket/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket
+{
+    public class User
+    {
+        private string ID;
+        private string Name;
+        public User(string ID, string Name) {
+            this.ID = ID;
+            this.Name = Name;
+        }
+
+        public string getID() { return this.ID; }
+        public string getName() { return this.Name;}
+    }
+}
