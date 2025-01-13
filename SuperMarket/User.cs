251ba26a13eabe99fdba7cf3d63b31367d94cfe2
= <=>
2025-01-13 23:42:46 +0300

    Added static method getName(id) which get the name of user by his id by accessing fike

--- SuperMarket/User.cs
+++ SuperMarket/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,23 @@ namespace SuperMarket
         public User(string ID, string Name) {
             this.ID = ID;
             this.Name = Name;
+        }
+
+        //gets the name of the user with id
+        public static string getName(string id)
+        {
+            StreamReader file  = File.OpenText(Constants.FileMethods.USER_FILE);
+            while (!file.EndOfStream)
+            {
+                string[] info = file.ReadLine().Split(',');
+                if (id == info[0])
+                {
+                    file.Close();
+                    return info[1];
+                }
+            }
+            file.Close();
+            return "";
         }
 
         public string getID() { return this.ID; }
